d731d1614d9cc212cc5070b3be58e4248ab61b54
felipelifa <felipe.delifa@gmail.com>
2025-09-21 14:27:31 -0300

    Deletei o arquivo main.js
    Foi um arquivo sem querer de JS, tirei ele, caso seja preciso no futuro coloco novamente.

--- main.js
+++
@@ -1,9 +0,0 @@
-const idade = Number (prompt("Digite sua idade")); {
-    if (idade => 18){
-        return "Maior de idade"
-    }
-} {
-    else (idade <= 19)
-        return "Menor de idade"   
-};
-console.log (idade);
