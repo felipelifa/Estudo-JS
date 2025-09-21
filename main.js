5a5a813c4b908185fa0790c7c5c14f650542d611
felipelifa <felipe.delifa@gmail.com>
2025-09-21 13:59:35 -0300

    Envio dos arquivos HTML
    Primeira versão do site de estudo para JS.

---
+++ main.js
@@ -0,0 +1,9 @@
+const idade = Number (prompt("Digite sua idade")); {
+    if (idade => 18){
+        return "Maior de idade"
+    }
+} {
+    else (idade <= 19)
+        return "Menor de idade"   
+};
+console.log (idade);
